045d3bbf59dd7c7a1a9ad6023933a41d270784b1
Christone007 <exellentemy@gmail.com>
2025-11-15 19:02:11 +0000

    Write the array of accounts required to create question

--- anchor_project/stack_underflow/programs/stack_underflow/src/instructions/create_question.rs
+++ anchor_project/stack_underflow/programs/stack_underflow/src/instructions/create_question.rs
@@ -3,12 +3,14 @@ use anchor_lang::prelude::*;
 use crate::errors::StackError;
 use crate::states::*;
 
-pub fn create_question(ctx: CreateQuestion, topic: String, body: String) -> Result<()> {
+pub fn create_question(ctx: Context<CreateQuestion>, topic: String, body: String) -> Result<()> {
     Ok(())
 }
 
 #[derive(Accounts)]
+#[instruction(topic: String)]
 pub struct CreateQuestion<'info> {
     pub question_authority: Signer<'info>,
     pub question: Account<'info, Question>,
+    pub system_program: Program<'info, System>,
 }
