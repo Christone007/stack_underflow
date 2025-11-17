0b51f6b200f468d2c8fce0609b902009a828c741
Christone007 <exellentemy@gmail.com>
2025-11-17 12:49:15 +0000

    Write tests for initializing questions

--- anchor_project/stack_underflow/programs/stack_underflow/src/instructions/post_answer.rs
+++ anchor_project/stack_underflow/programs/stack_underflow/src/instructions/post_answer.rs
@@ -4,11 +4,41 @@ use crate::errors::StackError;
 use crate::states::*;
 
 pub fn post_answer(ctx: Context<PostAnswer>, answer_body: String) -> Result<()> {
+    let answer_author = &ctx.accounts.answer_author;
+    let answer = &mut ctx.accounts.answer;
+    let root_question = &mut ctx.accounts.question;
+    let answer_body = answer_body;
+
+    // verify that answer body is not longer than allowed
+    require!(
+        answer_body.len() <= ANSWER_LENGTH,
+        StackError::AnswerTooLong
+    );
+
+    // set the values in the answer pda
+    answer.answer_author = answer_author.key();
+    answer.answer_body = answer_body;
+    answer.timestamp = Clock::get()?.unix_timestamp;
+
+    // increment the answer_count on the question
+    root_question.answer_count += 1;
+
     Ok(())
 }
 
 #[derive(Accounts)]
 pub struct PostAnswer<'info> {
+    #[account(mut)]
     pub answer_author: Signer<'info>,
+    #[account(
+        init,
+        payer=answer_author,
+        space= 8 + Answer::INIT_SPACE,
+        seeds = [ANSWER_SEED.as_bytes(), question.key().as_ref(), &question.answer_count.to_le_bytes()],
+        bump,
+    )]
     pub answer: Account<'info, Answer>,
+    #[account(mut)]
+    pub question: Account<'info, Question>,
+    pub system_program: Program<'info, System>,
 }
--- anchor_project/stack_underflow/tests/stack_underflow.ts
+++ anchor_project/stack_underflow/tests/stack_underflow.ts
@@ -3,7 +3,7 @@ import { Program } from "@coral-xyz/anchor";
 import { StackUnderflow } from "../target/types/stack_underflow";
 // import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 const QUESTION_SEED = "QUESTION";
 const ANSWER_SEED = "ANSWER";
@@ -16,12 +16,20 @@ describe("stack_underflow", () => {
   const program = anchor.workspace.stackUnderflow as Program<StackUnderflow>;
 
   const emeka = anchor.web3.Keypair.generate();
+  const alice = anchor.web3.Keypair.generate();
+  const bob = anchor.web3.Keypair.generate();
 
-  const question_1_topic = "PDA?";
+  const question_1_topic = "What is a PDA?";
   const question_1_body = "I came across something called PDAs while learning solana development. What exactly do they mean?";
 
+  const alice_question_topic = "What is ED25519 curve?rrrrrrrrrr"
+  const alice_question_body = "Someone should dive deep into the full mathematics behind secp256k1 from the prime field, short Weierstrass form, base point G, cofactor, all the way to how ECDSA signing/verification actually works u";
+
+  const bob_question_topic = "What is ED25519 curve?";
+  const bob_quesion_body = "Someone should dive deep into the full mathematics behind secp256k1 from the prime field, short Weierstrass form, base point G, cofactor, all the way to how ECDSA signing/verification actually works under the hood."
+
   describe("Create a Question", async () => {
-    it("Should successfully create a question with a valid topic and body", async () => {
+    it("Emeka: create a question with a valid topic and body", async () => {
       // airdrop emeka's wallet
       await airdrop(provider.connection, emeka.publicKey);
 
@@ -32,7 +40,7 @@ describe("stack_underflow", () => {
         emeka.publicKey.toBuffer(),
       ], program.programId);
 
-      console.log(`The Generated Question PDA is: ${question_pda} and its bump is ${question_bump}\n\n-> writing to PDA...`);
+      // console.log(`The Generated Question PDA is: ${question_pda} and its bump is ${question_bump}\n\n-> writing to PDA...`);
 
       // call the create_question method and provide it with the arguments, accounts, and signers and make it call the rpc
       await program.methods.initialize(question_1_topic, question_1_body).accounts(
@@ -45,6 +53,77 @@ describe("stack_underflow", () => {
 
       // check that the PDA contains the question
       await check_question(program, question_pda, emeka.publicKey, question_1_topic, question_1_body, 0);
+    })
+
+    it("Alice: create a question with topic and body length at max limits", async () => {
+      // airdrop alice's wallet
+      await airdrop(provider.connection, alice.publicKey);
+
+      // generate the question address deterministically
+      const [alice_question_pda, alice_question_bump] = PublicKey.findProgramAddressSync([
+        anchor.utils.bytes.utf8.encode(QUESTION_SEED),
+        anchor.utils.bytes.utf8.encode(alice_question_topic),
+        alice.publicKey.toBuffer(),
+      ], program.programId);
+
+      // call the create_question method and provide topic and body within the max_limit length
+      await program.methods.initialize(alice_question_topic, alice_question_body).accounts(
+        {
+          questionAuthority: alice.publicKey,
+          question: alice_question_pda,
+          systemProgram: anchor.web3.SystemProgram.programId
+        }
+      ).signers([alice]).rpc();
+
+      // confirm that the PDA contains the question
+      await check_question(program, alice_question_pda, alice.publicKey, alice_question_topic, alice_question_body, 0);
+
+    })
+
+    it("Fail to create a question when topic and/or body length limits are exceeded", async () => {
+      // airdrop wallet
+      await airdrop(provider.connection, bob.publicKey);
+
+      // find a program address for the question PDA
+      const [bob_question_pda] = PublicKey.findProgramAddressSync([
+        anchor.utils.bytes.utf8.encode(QUESTION_SEED),
+        anchor.utils.bytes.utf8.encode(bob_question_topic),
+        bob.publicKey.toBuffer(),
+      ], program.programId);
+
+      // call the create_question method with overflowing args
+      let txError;
+      try {
+        await program.methods.initialize(bob_question_topic, bob_quesion_body).accounts(
+          {
+            questionAuthority: bob.publicKey,
+            question: bob_question_pda,
+            systemProgram: anchor.web3.SystemProgram.programId
+          }
+        ).signers([bob]).rpc();
+
+        assert.fail("Transaction should have failed but it succeeded!");
+      } catch (err: any) {
+        txError = err;
+      }
+
+      // confirm if custom error was thrown
+      expect(txError.error.errorCode.code).to.equal("QuestionBodyTooLong");
+      expect(txError.error.errorMessage).to.include("Cannot initialize, Question body too long");
+
+      // confirm account was never created
+      const bob_q_acc = await provider.connection.getAccountInfo(bob_question_pda);
+      expect(bob_q_acc).to.be.null;
+    })
+  })
+
+  describe("Post Answers", async () => {
+    it("Emeka should be able to answer his own question", async () => {
+
+    })
+
+    it("Alice should be able to answer Emeka's question", async () => {
+
     })
   })
 
